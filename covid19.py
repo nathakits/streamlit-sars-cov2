6e3bc9a9e9f30c63dbc42dbe17830b39a2acdcd7
nathakits <nathakit.tan@gmail.com>
2021-01-28 00:01:54 +0700

    finish adding china analysis updated layout

--- covid19.py
+++ covid19.py
@@ -2,17 +2,20 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 from Bio import SeqIO
+from Bio.Seq import Seq
 from Bio import pairwise2
 
 """
 # Covid-19 Genome analysis
+This is a simple analysis of SARS-COV-2 complete genome samples from Thailand and China. The data is downloaded from [NCBI](https://www.ncbi.nlm.nih.gov/labs/virus/vssi/#/virus?SeqType_s=Protein&VirusLineage_ss=Severe%20acute%20respiratory%20syndrome%20coronavirus%202%20(SARS-CoV-2),%20taxid:2697049).
+
+Tools
+- BioPython
+- Streamlit
 """
 
 covid19th = SeqIO.read('./data/thailand/covid19-dna-complete.fasta', "fasta")
 covid19ch = SeqIO.read('./data/china/covid19-dna-complete.fasta', "fasta")
-st.write(f'The genome of the virus causing Covid-19 TH (known as SARS-CoV-2) consists of {len(covid19th)} genetic bases or letters.')
-st.write(f'The genome of the virus causing Covid-19 CH (known as SARS-CoV-2) consists of {len(covid19ch)} genetic bases or letters.')
-
 covid_DNA_th = covid19th.seq
 covid_DNA_ch = covid19ch.seq
 dna_th_200 = covid_DNA_th[:200]
@@ -31,11 +34,17 @@ if checkbox2:
 else:
     dnaLength2 = dna_ch_200
 
+'''
+## DNA sequence
+'''
+
 sequence = st.text_area("SARS-CoV-2 Thailand DNA Seqeunce", dnaLength1, height=30)
 sequence = st.text_area("SARS-CoV-2 China DNA Seqeunce", dnaLength2, height=30)
-
+st.write(f'Thailand DNA Seqeunce consists of {len(covid19th)} genetic bases or letters.')
+st.write(f'China DNA Seqeunce consists of {len(covid19ch)} genetic bases or letters.')
 
 '''
+## Nucleotides
 ### Nucleotides frequency
 Count the nucleotides frequency in the DNA
 '''
@@ -54,31 +63,27 @@ def getNucleotides(dna, nucleotides):
 
 getNucleotides(DNA_th, nucleotides_th)
 getNucleotides(DNA_ch, nucleotides_ch)
-st.json(nucleotides_th)
-st.json(nucleotides_ch)
 
-'''
-### Data frame
-Thailand
-'''
+col1, col2 = st.beta_columns(2)
+
+col1.write('Thailand')
 df1 = pd.DataFrame.from_dict(nucleotides_th, orient='index')
 df1 = df1.rename({0: 'Nucleotide Frequency'}, axis='columns')
 df1.reset_index(inplace=True)
 df1 = df1.rename(columns = {'index': 'Nucleotide'})
-st.write(df1)
-'''
-China
-'''
+col1.dataframe(df1)
+
+col2.write('China')
 df2 = pd.DataFrame.from_dict(nucleotides_ch, orient='index')
 df2 = df2.rename({0: 'Nucleotide Frequency'}, axis='columns')
 df2.reset_index(inplace=True)
 df2 = df2.rename(columns = {'index': 'Nucleotide'})
-st.write(df2)
+col2.dataframe(df2)
 
 '''
 ### Nucleotide chart
-Thailand
 '''
+st.write('Thailand')
 p1 = alt.Chart(df1).mark_bar().encode(
     x=alt.X('Nucleotide', sort='ascending'),
     y='Nucleotide Frequency',
@@ -86,9 +91,7 @@ p1 = alt.Chart(df1).mark_bar().encode(
 )
 st.altair_chart(p1, use_container_width=True)
 
-'''
-China
-'''
+st.write('China')
 p2 = alt.Chart(df2).mark_bar().encode(
     x=alt.X('Nucleotide', sort='ascending'),
     y='Nucleotide Frequency',
@@ -97,59 +100,74 @@ p2 = alt.Chart(df2).mark_bar().encode(
 st.altair_chart(p2, use_container_width=True)
 
 '''
+## mRNA
 ### Transcription
 Basically the mRNA is a copy of our DNA.
 However, in RNA, a base called uracil (U) replaces thymine (T) as the complementary
 nucleotide to adenine (that's the only difference, T is replaced by U).
 '''
-covid_mRNA = covid_DNA_th.transcribe()
-sequence = st.text_area("mRNA Seqeunce", covid_mRNA[:100], height=25)
+covid_mRNA_th = covid_DNA_th.transcribe()
+covid_mRNA_ch = covid_DNA_ch.transcribe()
+sequence = st.text_area("Thailand mRNA Seqeunce", covid_mRNA_th[:100], height=25)
+sequence = st.text_area("China mRNA Seqeunce", covid_mRNA_ch[:100], height=25)
 
 '''
 In order to see the difference, we align the covid-19 DNA and mRNA sequences.
-We can see that the mRNA is an identical copy with the T base replaced by U
+We can see that the mRNA is an identical copy with the T base replaced by U.
+In this example we show COVID-19 from Thailand.
 '''
 
-st.markdown(f'Covid-19 DNA: `{covid_DNA_th[:50]}`')
-st.markdown(f'Covid-19 RNA: `{covid_mRNA[:50]}`')
+st.markdown(f'Covid-19 TH DNA: `{covid_DNA_th[:50]}`')
+st.markdown(f'Covid-19 TH RNA: `{covid_mRNA_th[:50]}`')
 
 '''
+## Amino acids
 ### Translation
 Translation is the process that takes the information passed from DNA as
 messenger RNA and turns it into a series of amino acids.
 '''
-covid_aa = covid_mRNA.translate()
-sequence = st.text_area("mRNA Translation", covid_aa[:100], height=25)
-st.markdown(f"Covid-19's genome has {len(covid_aa)} amino acids")
 
-Proteins = covid_aa.split('*')
-st.markdown(f'We have {len(Proteins)} amino acid chains in the covid-19 genome')
+def pad_seq(sequence):
+    """ Pad sequence to multiple of 3 with N"""
+    remainder = len(sequence) % 3
+    return sequence if remainder == 0 else sequence + Seq('N' * (3 - remainder))
 
-for i in Proteins[:]:
-    if len(i) < 20:
-        Proteins.remove(i)
+aa_th = pad_seq(covid_mRNA_th).translate()
+aa_ch= pad_seq(covid_mRNA_ch).translate()
+proteins_th = aa_th.split('*')
+proteins_ch = aa_ch.split('*')
+st.text_area("Thailand mRNA Translation", aa_th[:100], height=25)
+st.text_area("China mRNA Translation", aa_ch[:100], height=25)
+st.markdown(f"TH Covid-19's genome has {len(aa_th)} amino acids and {len(proteins_th)} amino acid chains")
+st.markdown(f"CH Covid-19's genome has {len(aa_ch)} amino acids and {len(proteins_ch)} amino acid chains")
 
-st.markdown(f'We have {len(Proteins)} proteins with  more than 20 amino acids in the covid-19 genome')
-
-for i in Proteins[:]:
+for i in proteins_th[:]:
     if len(i) < 50:
-        Proteins.remove(i)
+        proteins_th.remove(i)
 
-st.markdown(f'We have {len(Proteins)} proteins with  more than 50 amino acids in the covid-19 genome')
+for i in proteins_ch[:]:
+    if len(i) < 50:
+        proteins_ch.remove(i)
 
-
-protein_clean = [str(i) for i in Proteins]
-df3 = pd.DataFrame({'amino_acids':protein_clean})
+col1, col2 = st.beta_columns(2)
+protein_clean_th = [str(i) for i in proteins_th]
+df3 = pd.DataFrame({'amino_acids':protein_clean_th})
 df3['count'] = df3['amino_acids'].str.len()
 df3.head()
-st.write(df3)
+col1.write(df3)
+col1.markdown(f'We have {len(proteins_th)} proteins with more than 50 amino acids from the Thailand covid-19 genome')
+
+protein_clean_ch = [str(i) for i in proteins_ch]
+d4 = pd.DataFrame({'amino_acids':protein_clean_ch})
+d4['count'] = d4['amino_acids'].str.len()
+d4.head()
+col2.write(d4)
+col2.markdown(f'We have {len(proteins_ch)} proteins with more than 50 amino acids from the China covid-19 genome')
 
 '''
-### Compare Thailand and China strain
+## Comparing Thailand and China strain
+Using pairwise2 to compare sequence alignment in order to identify similarities.
 '''
-# covid_DNA_th
-# covid_DNA_ch
 
-# comparison_score = pairwise2.align.globalxx(covid_DNA_th, covid_DNA_ch, one_alignment_only=True, score_only=True)
-# st.write(f'{len(covid_DNA_th)} + {comparison_score} = ', len(covid_DNA_th) - comparison_score)
-# st.write('SARS/COV Similarity (%):', comparison_score / len(covid_DNA_th) * 100)
+alignment_score = pairwise2.align.globalxx(covid_DNA_th, covid_DNA_ch, one_alignment_only=True, score_only=True)
+st.write('COV-TH/COV-CH Similarity (%):', alignment_score / len(covid_DNA_th) * 100)
